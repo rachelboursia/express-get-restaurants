4b299ee8a62d518b6fa158e5daf0f96863d4fb12
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-03-09 13:39:47 -0800

    Completed express get restaurants part 2

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const {sequelize} = require("./db");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below
 
+        // Part 1: 
 app.get("/restaurants", (req, res) => {
     Restaurant.findAll().then(restaurant => {
+        res.json(restaurant);
+    });
+});
+        // Part 2: 
+app.get("/restaurants/:id", (req, res) => {
+    Restaurant.findByPk(req.params.id).then(restaurant => {
         res.json(restaurant);
     });
 });
